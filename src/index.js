1de426c576ba2ae1a06c1bb130fe7e18dc181b3a
xpeper <xpepermint@gmail.com>
2016-10-05 12:42:32 +0200

    timeout method

---
+++ src/index.js
@@ -0,0 +1,21 @@
+/*
+* A timeout helper function resolves the provider promise but rejects if the
+* operation takes longer then the provided `timeout`.
+*/
+
+exports.timeout = function(promise, {time=0, error=new Error()}={}) {
+  let timer = null;
+
+  let sleep = time > 0
+    ? new Promise((resolve, reject) => timer = setTimeout(reject, time, error))
+    : null;
+
+  let run = promise.then((value) => {
+    clearTimeout(timer);
+    return value;
+  });
+
+  return Promise.race(
+    [run, sleep].filter(p => !!p)
+  );
+}
